8bbc3aa5976b12313f048be1b34519cb2ccef668
Daniel de Souza Iseri <danielsi-2015@hotmail.com>
2022-10-28 09:42:26 -0300

    add filter

--- src/routes.js
+++ src/routes.js
@@ -1,75 +1,106 @@
-const routes = require('express').Router();
+const routes = require("express").Router();
 
-routes.get('/hard-disc', (req, res)=>{
-  const payload = require('./repositories/hard-disc.json'); 
+routes.get("/hard-disc/:id", (req, res) => {
+  const payload = require("./repositories/hard-disc.json");
+  const { id } = req.params;
 
-  if(!payload)
-    return res.status(400).send('not found');
+  if (!payload) return res.status(400).send("not found");
 
-  return res.send(payload);
-})
-
-routes.get('/processor', (req, res)=>{
-  const payload = require('./repositories/processor.json');;
-
-  if(!payload)
-    return res.status(400).send('not found');
+  if (id == 3 || id == 4)
+    return res.send(
+      payload.filter(({ description }) => description.includes("SSD"))
+    );
 
   return res.send(payload);
-})
+});
 
-routes.get('/video-card', (req, res)=>{
-  const payload = require('./repositories/video-card.json');
+routes.get("/processor/:id", (req, res) => {
+  const payload = require("./repositories/processor.json");
+  const { id } = req.params;
 
-  if(!payload)
-    return res.status(400).send('not found');
+  if (!payload) return res.status(400).send("not found");
+
+  if (id == 3)
+    return res.send(
+      payload.filter(({ description }) => description.includes("i5"))
+    );
+
+  if (id == 4)
+    return res.send(
+      payload.filter(({ description }) => description.includes("i7"))
+    );
 
   return res.send(payload);
-})
+});
 
-routes.get('/memory', (req, res)=>{
-  const payload = require('./repositories/memory.json');
+routes.get("/video-card/:id", (req, res) => {
+  const payload = require("./repositories/video-card.json");
+  const { id } = req.params;
 
-  if(!payload)
-    return res.send('not found');
+  if (!payload) return res.status(400).send("not found");
 
-  return res.status(400).send(payload);
-})
+  if (id > 2) return res.send(payload);
 
-routes.get('/desktop', (req, res)=>{
-  const payload = require('./repositories/desktop.json');
+  return res.send();
+});
 
-  if(!payload)
-    return res.send('not found');
+routes.get("/memory/:id", (req, res) => {
+  const payload = require("./repositories/memory.json");
+  const { id } = req.params;
 
-  return res.status(400).send(payload);
-})
+  if (!payload) return res.send("not found");
 
-routes.get('/cooler', (req, res)=>{
-  const payload = require('./repositories/cooler.json');
+  if (id == 2)
+    return res.send(
+      payload.filter(({ description }) => description.includes("4GB"))
+    );
 
-  if(!payload)
-    return res.send('not found');
+  if (id == 3)
+    return res.send(
+      payload.filter(({ description }) => description.includes("8GB"))
+    );
 
-  return res.status(400).send(payload);
-})
+  if (id == 4)
+    return res.send(
+      payload.filter(
+        ({ description }) =>
+          description.includes("32GB") || description.includes("16GB")
+      )
+    );
 
-routes.get('/font', (req, res)=>{
-  const payload = require('./repositories/font.json');
+  return res.send(payload);
+});
 
-  if(!payload)
-    return res.send('not found');
+routes.get("/desktop", (req, res) => {
+  const payload = require("./repositories/desktop.json");
 
-  return res.status(400).send(payload);
-})
+  if (!payload) return res.status(400).send("not found");
 
-routes.get('/motherboard', (req, res)=>{
-  const payload = require('./repositories/motherboard.json');
+  return res.send(payload);
+});
 
-  if(!payload)
-    return res.send('not found');
+routes.get("/cooler", (req, res) => {
+  const payload = require("./repositories/cooler.json");
 
-  return res.status(400).send(payload);
-})
+  if (!payload) return res.status(400).send("not found");
+
+  return res.send(payload);
+});
+
+routes.get("/font", (req, res) => {
+  const payload = require("./repositories/font.json");
+
+  if (!payload) return res.send("not found");
+
+  return res.send(payload);
+});
+
+routes.get("/motherboard", (req, res) => {
+  const payload = require("./repositories/motherboard.json");
+
+  if (!payload) return res.send("not found");
+
+  return res.send(payload);
+});
 
 module.exports = routes;
