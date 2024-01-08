c4ffad88bdebab3cde017908fe3ef5b72ad8e05a
Daniel Iseri <danielsi-2015@hotmail.com>
2024-01-07 23:36:12 -0300

    FIX - Remove require formattedImage

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const formattedImage = require('../utils/formattedImage');
 
 ; (async () => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
